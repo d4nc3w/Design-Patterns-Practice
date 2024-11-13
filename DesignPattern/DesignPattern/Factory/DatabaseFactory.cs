2298ebb43fc21124d0eef6155f1dad6af7e1d76b
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-11-13 16:38:08 +0100

    Implemented Factory Design Pattern

--- DesignPattern/DesignPattern/Factory/DatabaseFactory.cs
+++ DesignPattern/DesignPattern/Factory/DatabaseFactory.cs
@@ -2,9 +2,18 @@
 {
     public static class DatabaseFactory
     {
-        //public static IDatabase CreateDatabase(...)
-        //{
-        //  ...
-        //}
+        public static IDatabase CreateDatabase(DatabaseType dbType)
+        {
+            if (dbType == DatabaseType.SqlServer)
+            {
+                return new SqlServerDatabase();
+            }
+
+            if (dbType == DatabaseType.Oracle)
+            {
+                return new OracleDatabase();
+            }
+            throw new Exception("Unknown database type");
+        }
     }
 }
--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -10,10 +10,10 @@ class Program
         dbEngines();
         Thread.Sleep(2000);
         Console.WriteLine();
-        employeesAdapterDemo();
+        //employeesAdapterDemo();
         Thread.Sleep(2000);
         Console.WriteLine();
-        observingThings();
+        //observingThings();
     }
 
     // No need to replace this code, it should work after implementing proper factory design pattern
@@ -27,47 +27,47 @@ class Program
     }
 
     // No need to replace this code, just implement the adapter class for it to work properly
-    static void employeesAdapterDemo()
-    {
-        string[,] employeesArray = new string[5, 4]
-        {
-                {"101","John","SE","10000"},
-                {"102","Smith","SE","20000"},
-                {"103","Dev","SSE","30000"},
-                {"104","Pam","SE","40000"},
-                {"105","Sara","SSE","50000"}
-        };
-
-        Console.WriteLine("HR system passes employee string array to Adapter\n");
-        ITarget target = new EmployeeAdapter();
-        target.ProcessCompanySalary(employeesArray);
-    }
+    // static void employeesAdapterDemo()
+    // {
+    //     string[,] employeesArray = new string[5, 4]
+    //     {
+    //             {"101","John","SE","10000"},
+    //             {"102","Smith","SE","20000"},
+    //             {"103","Dev","SSE","30000"},
+    //             {"104","Pam","SE","40000"},
+    //             {"105","Sara","SSE","50000"}
+    //     };
+    //
+    //     Console.WriteLine("HR system passes employee string array to Adapter\n");
+    //     ITarget target = new EmployeeAdapter();
+    //     target.ProcessCompanySalary(employeesArray);
+    // }
 
     // No need to replace this code, just implement the ConcreteObserver class for it to work properly
-    static void observingThings()
-    {
-        Subject RedMI = new("Red MI Mobile", 10000, "Out Of Stock");
-
-        ConcreteObserver user1 = new("Anurag");
-        user1.AddSubscriber(RedMI);
-
-        ConcreteObserver user2 = new("Pranaya");
-        user2.AddSubscriber(RedMI);
-
-        ConcreteObserver user3 = new("Priyanka");
-        user3.AddSubscriber(RedMI);
-
-        Thread.Sleep(1000);
-
-        Console.WriteLine("Red MI Mobile current state : " + RedMI.GetAvailability());
-        Console.WriteLine();
-
-        Thread.Sleep(1000);
-
-        user3.RemoveSubscriber(RedMI);
-
-        Thread.Sleep(1000);
-        RedMI.SetAvailability("Available");
-    }
+    // static void observingThings()
+    // {
+    //     Subject RedMI = new("Red MI Mobile", 10000, "Out Of Stock");
+    //
+    //     ConcreteObserver user1 = new("Anurag");
+    //     user1.AddSubscriber(RedMI);
+    //
+    //     ConcreteObserver user2 = new("Pranaya");
+    //     user2.AddSubscriber(RedMI);
+    //
+    //     ConcreteObserver user3 = new("Priyanka");
+    //     user3.AddSubscriber(RedMI);
+    //
+    //     Thread.Sleep(1000);
+    //
+    //     Console.WriteLine("Red MI Mobile current state : " + RedMI.GetAvailability());
+    //     Console.WriteLine();
+    //
+    //     Thread.Sleep(1000);
+    //
+    //     user3.RemoveSubscriber(RedMI);
+    //
+    //     Thread.Sleep(1000);
+    //     RedMI.SetAvailability("Available");
+    // }
 
 }
